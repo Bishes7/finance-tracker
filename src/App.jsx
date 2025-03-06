c03cc0579166068361cd169673144a4a2641f699
Bishes7 <specialbishes9@gmail.com>
2025-03-07 02:47:48 +1100

    react bootstrap imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import { Button } from "react-bootstrap";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Button>Click</Button>
+    </>
+  );
 }
 
 export default App;
